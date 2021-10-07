954c3e07d4c893f7037ef48a6cc6e02620b99527
danilmv <43599357+danilmv@users.noreply.github.com>
2021-10-07 21:46:36 +0300

    L2: MVP for UserItem removed :)

--- app/src/main/java/com/andriod/githubapiapp/userlist/UserListAdapter.kt
+++ app/src/main/java/com/andriod/githubapiapp/userlist/UserListAdapter.kt
@@ -2,45 +2,36 @@ package com.andriod.githubapiapp.userlist
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.view.children
-import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.andriod.githubapiapp.databinding.ItemUserBinding
 import com.andriod.githubapiapp.entity.User
 
 class UserListAdapter : RecyclerView.Adapter<UserListAdapter.ViewHolder>() {
 
-    private val presenter by lazy { UserItemPresenter() }
+    private var users = mutableListOf<User>()
 
     class ViewHolder(parent: ViewGroup) :
         RecyclerView.ViewHolder(
             ItemUserBinding.inflate(
                 LayoutInflater.from(parent.context), parent, false
             ).root
-        ), UserItemContract.View {
+        ) {
         private var binding: ItemUserBinding = ItemUserBinding.bind(itemView)
 
-        override fun setState(state: UserItemContract.ViewState) {
-            binding.root.children.forEach { it.isVisible = false }
-            when (state) {
-                UserItemContract.ViewState.IDLE -> binding.itemContainer.isVisible = true
-                UserItemContract.ViewState.LOADING -> binding.progressBar.isVisible = true
-            }
-        }
-
-        override fun setUser(user: User) {
+        fun onBind(user: User) {
             binding.loginTextView.text = user.login
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(parent)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) =
-        presenter.onBind(holder, position)
+        holder.onBind(users[position])
 
-    override fun getItemCount() = presenter.getItemCount()
+    override fun getItemCount() = users.size
     fun setData(users: List<User>?) {
         users?.let {
-            presenter.passUserList(users)
+            this.users.clear()
+            this.users.addAll(users)
             notifyDataSetChanged()
         }
     }
